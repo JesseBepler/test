385e229c03d309c5cc6c5f0d355ac030a418805a
Jesse Bepler <Bepler94@gmail.com>
2024-11-08 12:03:01 -0800

    Add server.js to set up the game server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files (e.g., index.html, script.js, style.css)
+app.use(express.static(path.join(__dirname, '/')));
+
+// Basic Socket.IO connection event
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+// Start the server on port 3000
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
